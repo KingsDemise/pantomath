de749385d879fc2b1cf622c86445937b863635f4
Kingshuk Kirtania <bttb.kingshukk@gmail.com>
2023-08-13 14:09:18 +0530

    first commit

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Pantomath</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Know Everything" />
+      <link rel="icon" href="/favicon.svg" />
+    </>
+  );
+}
