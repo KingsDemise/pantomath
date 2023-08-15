c9700d9db1e971e020c3d45e0a819f615e493b8f
KingsDemise <bttb.kingshukk@gmail.com>
2023-08-15 16:12:03 +0530

    further changes to search header

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AiOutlineCamera, AiOutlineSearch } from "react-icons/ai";
+import { BsCardImage } from "react-icons/bs";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 export default function SearchHeaderOptions() {
   const pathname = usePathname();
@@ -16,8 +17,8 @@ export default function SearchHeaderOptions() {
     <div className="flex space-x-2 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm">
       <div
         onClick={() => selectTab("All")}
-        className={`hover:brightness-105 hover:shadow-md flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
-          pathname === "/search/web" && "!text-blue-600 !border-blue-600"
+        className={`hover:brightness-105 hover:shadow-md flex items-center space-x-1 border-b-4 border-transparent active:text-red-500 cursor-pointer pb-3 px-2 ${
+          pathname === "/search/web" && "!text-red-600 !border-red-600"
         }`}
       >
         <AiOutlineSearch className="text-md" />
@@ -25,11 +26,11 @@ export default function SearchHeaderOptions() {
       </div>
       <div
         onClick={() => selectTab("Images")}
-        className={`hover:brightness-105 hover:shadow-md flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
-          pathname === "/search/image" && "!text-blue-600 !border-blue-600"
+        className={`hover:brightness-105 hover:shadow-md flex items-center space-x-1 border-b-4 border-transparent active:text-red-500 cursor-pointer pb-3 px-2 ${
+          pathname === "/search/image" && "!text-red-600 !border-red-600"
         }`}
       >
-        <AiOutlineCamera className="text-md" />
+        <BsCardImage className="text-md" />
         <p>Images</p>
       </div>
     </div>
